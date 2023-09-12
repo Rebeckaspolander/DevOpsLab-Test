f84454b7dec19fa7a25884056de1c6ac0282653e
rebeckaspolander <rebeckaspolander@yahoo.se>
2023-09-12 08:57:20 +0200

    Initial commit

---
+++ src/main/java/com/example/DevOpsLabTest/DevOpsLabTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsLabTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsLabTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsLabTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOpsLabTest/DevOpsLabTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsLabTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsLabTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
